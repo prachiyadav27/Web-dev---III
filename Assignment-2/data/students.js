5369d3ef5992dd4cf55f7fda8ce62a598f4ea8aa
prachiyadav27 <ydvprachi8486@gmail.com>
2026-09-19 15:54:34 +0530

    Assignment-2

---
+++ Assignment-2/data/students.js
@@ -0,0 +1,22 @@
+let students = [
+    {
+        id: 1,
+        name: "Rahul",
+        course: "BCA",
+        age: 20
+    },
+    {
+        id: 2,
+        name: "Priya",
+        course: "B.Tech",
+        age: 19
+    },
+    {
+        id: 3,
+        name: "Amit",
+        course: "BCA",
+        age: 21
+    }
+];
+
+module.exports = students;
